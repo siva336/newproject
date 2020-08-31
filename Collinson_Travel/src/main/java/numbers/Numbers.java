dd54e5bf4968412c3817aa0d014676d16d9610ae
siva336 <70444603+siva336@users.noreply.github.com>
2020-08-31 10:01:53 +0530

    Add files via upload

---
+++ Collinson_Travel/src/main/java/numbers/Numbers.java
@@ -0,0 +1,11 @@
+package numbers;
+
+public class Numbers {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 1; i++)
+        {
+            System.out.println(i);
+        }
+
+    }
+}
